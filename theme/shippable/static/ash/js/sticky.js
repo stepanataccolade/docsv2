d2fae50884d6a379bd36e61d66a64fd76e4057b2
Sam <xdothackerx@gmail.com>
2015-06-24 11:37:13 -0700

    Fix side nav overlapping footer when page is scrolled to the bottom (#102).

--- theme/shippable/static/ash/js/sticky.js
+++ theme/shippable/static/ash/js/sticky.js
@@ -1,8 +1,12 @@
+$( document ).ready( function() {
+    var sidebar_bottom = $('.tocbar').position().top+$('.tocbar').outerHeight(true);
+    $('.sticky-anchor').css("top", sidebar_bottom)
+})
+
 function sticky_relocate() {
     var footer_top = $('#footer.bg').offset().top;
-    var page_bottom = $(window).scrollTop() + $(window).height();
-    var margin = $('#article').height() - $('.tocbar').height();
-    if (page_bottom > footer_top) {
+    var anchor_bottom = $('.sticky-anchor').offset().top+$('.sticky-anchor').outerHeight(true);
+    if (anchor_bottom > footer_top) {
         $('#sidebar .tocbar').addClass('stick');
         $('#sidebar').height( $('#mainbar').height() );
     } else {
