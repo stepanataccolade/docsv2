2dee2d863a8ed6078542e3ed3bb531708c6ebdb9
xdothackerx <xdothackerx@gmail.com>
2016-03-25 12:13:21 -0700

    Add preventDefault to search-redirect.js

--- theme/shippable/static/ash/js/search-redirect.js
+++ theme/shippable/static/ash/js/search-redirect.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
-  $('#mkdocs-search-query').submit(function () {
+  $('#mkdocs-search-query').on('submit', function(e) {
+    e.preventDefault();
     var q = $('#mkdocs-search-query').val();
     alert("Searched " + q);
     window.location.href = "http://docs.shippable.com/search.html?q=" + q;
   })
- });
+});
