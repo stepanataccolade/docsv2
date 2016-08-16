c490411238eff6847745b3eacdae159a1eb23ab5
xdothackerx <xdothackerx@gmail.com>
2016-08-15 19:24:22 -0700

    Style page body with left nav.

--- theme/shippable/static/ash/js/accordion.js
+++ theme/shippable/static/ash/js/accordion.js
@@ -1,10 +1,5 @@
 $(document).ready(function () {
-  $('#side-nav > li > a').mouseover(function(){
-    if ($(this).attr('class') != 'active'){
-      $('#side-nav li ul').slideUp();
-      $(this).next().slideToggle();
-      $('#side-nav li a').removeClass('active');
-      $(this).addClass('active');
-    }
+  $('#side-nav h4').click(function(){
+    $(this).next('ul').toggleClass('active');
   });
 });
